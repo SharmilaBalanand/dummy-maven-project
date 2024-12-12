2a7c99bdc07dfb68cd2402ab238d9615937893c7
Sharmila Balanand <98818762+SharmilaBalanand@users.noreply.github.com>
2024-12-12 13:55:46 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,10 +4,13 @@ package com.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        try {
+            Thread.sleep(Long.MAX_VALUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
